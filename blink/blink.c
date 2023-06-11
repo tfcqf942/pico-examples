e087dc677476b14da8e7da95f564cb1cda1a8838
Andrew Scheller <andrew.scheller@raspberrypi.com>
2023-06-11 21:15:45 +0100

    Fix broken link in README.md (#397)

--- blink/blink.c
+++ blink/blink.c
@@ -10,12 +10,13 @@ int main() {
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
 #else
+    timer_hw->dbgpause = 0;
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        sleep_ms(250);
+        sleep_ms(1250);
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
     }
